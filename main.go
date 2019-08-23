3551bce8b5776f91fe6636c4e772c6ec363c6053
Nurahmadie <nurahmadie@gmail.com>
2019-08-23 11:36:10 +0700

    Remove tab in docs

--- main.go
+++ main.go
@@ -8,18 +8,19 @@
 // query result.
 //
 // Usage examples:
-// 		$ socks5dns -s example.com:22 -b localhost:53 -r /etc/socks5dns/resolv.txt
+//      $ socks5dns -s example.com:22 -b localhost:53
 //
 // Options:
-//		-b=<127.0.0.1:53>       Bind to this host and port. Default to 127.0.0.1:53.
-//		-c                      Enable caching.
-//		-i=<$HOME/.ssh/id_rsa>  Specify identity file to use when connecting to ssh server.
-//		-r=<./resolv.conf>      Specify a file for list of DNS to use. Default to ./resolv.conf.
-//		-s=<127.0.0.1:22>       Connect to this ssh server. Default to 127.0.0.1:22.
-//		-u=<$USER>              Specify user to connect with ssh server.
-//		-t=<30>                 Duration before connection timeout, in second. Default to 30 seconds.
-//		-dns=<8.8.8.8:53>       Remote DNS server to connect to,
+//      -b=<127.0.0.1:53>       Bind to this host and port. Default to 127.0.0.1:53.
+//      -c                      Enable caching.
+//      -i=<$HOME/.ssh/id_rsa>  Specify identity file to use when connecting to ssh server.
+//      -s=<127.0.0.1:22>       Connect to this ssh server. Default to 127.0.0.1:22.
+//      -u=<$USER>              Specify user to connect with ssh server.
+//      -t=<30>                 Duration before connection timeout, in second. Default to 30 seconds.
+//      -dns=<8.8.8.8:53>       Remote DNS server to connect to,
 //                              target server should accept TCP connection. Default to 8.8.8.8:53.
+//      -w=<# of CPU>           Number of workers to run. Default to the number of CPU.
+//      -h=<>                   Specify hostkey to verify whether ssh server is trusted or not.
 //
 package main
 
