780c2dcf4c45890d50a913c049e7a116e880beb1
Nurahmadie <177826+fudanchii@users.noreply.github.com>
2023-09-25 08:50:20 +0700

    Update README

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ Usage of ./ssh2dns:
 | --- | --- |
 | `-b string` | Bind to this host and port, default to 127.0.0.1:53 (default "127.0.0.1:53") |
 | `-c` | Use cache, default to false |
+| `-dns` | Use the given dns server to connet, takes no effect if `-x` is set (default "8.8.8.8:53") |
 | `-h string` | Specify hostkey to use with ssh server (default "$HOME/.ssh/known_hosts")
 | `-i string` | Specify identity file to use when connecting to ssh server (default "$HOME/.ssh/id_rsa") |
+| `-r` | Do recursive lookup with the default root servers hint, if set, `-dns` option will take no effect. Default to false. |
 | `-s string` | Connect to this ssh server, default to 127.0.0.1:22 (default "127.0.0.1:22") |
 | `-t int` | Set timeout for net dial, default to 30 seconds (default 30) |
 | `-u string` | Specify user to connect with ssh server (default "$USER") |
--- cmd/ssh2dns/main.go
+++ cmd/ssh2dns/main.go
@@ -11,18 +11,8 @@
 //
 //	$ ssh2dns -s example.com:22 -b localhost:53
 //
-// Options:
-//
-//	-b=<127.0.0.1:53>       Bind to this host and port. Default to 127.0.0.1:53.
-//	-c                      Enable caching.
-//	-i=<$HOME/.ssh/id_rsa>  Specify identity file to use when connecting to ssh server.
-//	-s=<127.0.0.1:22>       Connect to this ssh server. Default to 127.0.0.1:22.
-//	-u=<$USER>              Specify user to connect with ssh server.
-//	-t=<30>                 Duration before connection timeout, in second. Default to 30 seconds.
-//	-dns=<8.8.8.8:53>       Remote DNS server to connect to,
-//	                        target server should accept TCP connection. Default to 8.8.8.8:53.
-//	-w=<# of CPU>           Number of workers to run. Default to the number of CPU.
-//	-h=<>                   Specify hostkey to verify whether ssh server is trusted or not.
+// See ssh2dns -help for available options.
+
 package main
 
 import (
